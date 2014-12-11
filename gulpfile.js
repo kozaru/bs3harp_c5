85fe563f3a600650d2335c9a4e41b2ae1066ac3f
kozaru <nobuko@corogari.com>
2014-12-11 15:28:29 +0900

    Add gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var copy = require('gulp-copy');
 var prettify = require('gulp-html-prettify');
+var imagemin = require('gulp-imagemin');
+var newer = require('gulp-newer');
 var runSequence = require('run-sequence');
 
 var config = {
@@ -9,35 +11,73 @@ var config = {
   'sourceHTML': './www/html/**',
   'sourceCSS': './www/css/**',
   'sourceJS': './www/js/**',
+  'sourceIMG': './www/images/**',
   'distHTML': '../html/',
   'distCSS': '../css/',
   'distJS': '../js/',
+  'distIMG': '../images/',
   'bsLESS': './bower/bootstrap/less/**',
   'bsJS': './bower/bootstrap/dist/js/bootstrap.js',
   'bsJSmin': './bower/bootstrap/dist/js/bootstrap.min.js',
   'bsJQUERY': './bower/jquery/dist/**',
-  'publicLESS': './css/_bs/',
-  'publicJS': './js/'
+  'publicLESS': './public/css/_bs/',
+  'publicJS': './public/js/'
 }
 
-gulp.task('bs', function() {
-  gulp.src(config.bsLESS)
+gulp.task('bsless', function() {
+  return gulp.src(config.bsLESS)
   .pipe(gulp.dest(config.publicLESS));
-  gulp.src(config.bsJS)
-  .pipe(gulp.dest(config.publicJS));
-  gulp.src(config.bsJSmin)
-  .pipe(gulp.dest(config.publicJS));
-  gulp.src(config.bsJQUERY)
+});
+
+gulp.task('bsjs', function() {
+  return gulp.src(config.bsJS)
   .pipe(gulp.dest(config.publicJS));
 });
 
-gulp.task('copy', function() {
-  gulp.src(config.sourceHTML)
+gulp.task('bsjquery', function() {
+  return gulp.src(config.bsJQUERY)
+  .pipe(gulp.dest(config.publicJS));
+});
+
+gulp.task('bs', function() {
+  runSequence(
+    'bsless',
+    'bsjs',
+    'bsjquery'
+    );
+});
+
+gulp.task('copyhtml', function() {
+  return gulp.src(config.sourceHTML)
   .pipe(gulp.dest(config.distHTML));
-  gulp.src(config.sourceCSS)
+});
+
+gulp.task('copycss', function() {
+  return gulp.src(config.sourceCSS)
   .pipe(gulp.dest(config.distCSS));
-  gulp.src(config.sourceJS)
+});
+
+gulp.task('copyjs', function() {
+  return gulp.src(config.sourceJS)
   .pipe(gulp.dest(config.distJS));
+});
+
+gulp.task('copyimg', function() {
+  return gulp.src(config.sourceIMG)
+    .pipe(newer(config.distIMG))
+    .pipe(imagemin({
+      optimizationLevel: 3
+    })) // See gulp-imagemin page.
+    .pipe(gulp.dest(config.distIMG));
+});
+
+gulp.task('copy', function() {
+  runSequence(
+    'copyhtml',
+    'copycss',
+    'copyjs',
+    'copyimg'
+    );
 });
 
 gulp.task('pretty', function() {
@@ -46,7 +86,7 @@ gulp.task('pretty', function() {
     indent_char: ' ',
     indent_size: 2
   }))
-  .pipe(gulp.dest(config.sourceMinify))
+  .pipe(gulp.dest(config.sourceMinify));
 });
 
 gulp.task('dist', function() {
